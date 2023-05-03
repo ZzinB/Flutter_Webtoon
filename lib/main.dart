afc4e8eb89beac7e1d11edfd932b7932b5d9eea0
16shinbi <21600510@handong.edu>
2023-05-03 21:10:40 +0900

    toolbar

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,43 @@ class App extends StatelessWidget {
     return MaterialApp(
       //Android
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello'),
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40), //화면 상하좌우에 패딩 10
+          child: Column(
+            //수직
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                //수평
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
